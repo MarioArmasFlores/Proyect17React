50c86cfab508782c100a5c66533f77293a8f70e3
practicasIntermedio <doscaras51@gmail.com>
2023-07-24 17:17:25 -0500

    nuevosCambios,proyecto totalmente diferente al iniciado

--- Ejercicio17React/src/App.tsx
+++ Ejercicio17React/src/App.tsx
@@ -1,16 +1,35 @@
 
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from "./components/NewNotes";
+
 
 function App() {
- 
+
+
+
+
 
   return (
-    <>
-      <h1>Hola</h1>
-      
-       
-    </>
+
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote/>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container> 
+
+
+
+
+
   )
 }
 
-export default App
+export default App;
